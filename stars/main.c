b55bf4449f34e4316ff0e6ffa49d1df82035e008
Qusai Zahalqa <QusaiZahalqa@gmail.com>
2015-08-15 17:25:55 +0300

    Created by Coding Ground Client

---
+++ stars/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+void stars(int numStars)
+{
+    int i;
+    for(i = 0; i < numStars; ++i)
+    {
+        printf("* ");
+    }
+    printf("\n");
+}
+
+void square(int side)
+{
+    int i;
+    for(i = 0; i < side; ++i)
+    {
+       stars(side);
+    }
+}
+
+void triangle(int hight)
+{
+    int i;
+    int numStars;
+    numStars = 0;
+    for(i = 0; i < hight; ++i)
+    {
+        ++numStars;
+        stars(numStars);
+    }
+}
+
+void reverseTriangle(int hight)
+{
+    int i;
+    int numStars;
+    numStars = hight;
+    for(i = 0; i < hight; ++i)
+    {
+        stars(numStars);
+        --numStars;
+    }
+}
+
+int main()
+{
+    int rows;
+    int counter;
+    
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    
+    triangle((rows + 1) / 2);
+    reverseTriangle((rows + 1) / 2 - 1);
+ 
+    
+    return 0;
+
+}
